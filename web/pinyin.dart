71df83fe7f17ba4cba41ce7c925f799b6d317b96
kpmy <petryxa.clever@gmail.com>
2016-05-20 00:21:44 +0300

    больше слогов + перевод-подсказка + скрытие после 3х повторений

--- web/pinyin.dart
+++ web/pinyin.dart
@@ -5,15 +5,17 @@ import 'dart:async';
 
 var rnd = new Random();
 var x = querySelector("#text");
+var tip = querySelector("#tip");
 var a = querySelector("#a");
 var b = querySelector("#b");
 var c = querySelector("#c");
 
-void nextSym(){
-
+var data = [];
+void next(){
   var next = rnd.nextInt(data.length);
   x.attributes["data-answer"] = next.toString();
   x.text = data[next][datasym];
+  tip.text = data[next][datatip];
 
   var variants = [];
   variants.length = 3;
@@ -22,6 +24,11 @@ void nextSym(){
   for(var i = 0; i<3;){
     if (i == correct) {
       i++;
+    } else if (data.length == 1){
+      variants[0] = correct;
+      variants[1] = correct;
+      variants[2] = correct;
+      break;
     } else {
       var v = rnd.nextInt(data.length);
       if (v != next) {
@@ -38,6 +45,29 @@ void nextSym(){
   c.attributes["data-answer"] = variants[2].toString();
 }
 
+Map<String, int> score = {};
+
+void initScore(){
+  for(var i = 0; i<dict.length; i++){
+    score[dict[i][datasym]] = 0;
+  }
+}
+
+void nextSym(){
+  data = [];
+  for(var i = 0; i<dict.length; i++){
+    if(score[dict[i][datasym]]<2){
+      data.add(dict[i]);
+    }
+  }
+  if(data.length>0){
+    next();
+  }else{
+    x.text = ".";
+    tip.text = "";
+  }
+}
+
 void reset(){
   x.attributes["style"] = "color: black;";
   nextSym();
@@ -45,7 +75,8 @@ void reset(){
 
 const duration = const Duration(milliseconds: 500);
 
-void correct(){
+void correct(variant){
+  score[data[variant][datasym]]++;
   x.attributes["style"] = "color: green;";
   new Timer(duration, reset);
 }
@@ -56,20 +87,20 @@ void incorrect(){
 }
 
 void answer(int variant){
-  print(variant);
-  print(int.parse(x.attributes["data-answer"]));
+//  print(variant);
+//  print(int.parse(x.attributes["data-answer"]));
   if (int.parse(x.attributes["data-answer"]) == variant) {
-    correct();
+    correct(variant);
   } else {
     incorrect();
   }
 }
 
 void main() {
-
   a.onClick.listen((e) => answer(int.parse(a.attributes["data-answer"])));
   b.onClick.listen((e) => answer(int.parse(b.attributes["data-answer"])));
   c.onClick.listen((e) => answer(int.parse(c.attributes["data-answer"])));
 
+  initScore();
   nextSym();
 }
